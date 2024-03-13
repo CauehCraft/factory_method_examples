8f0a291b611b7b4c7f4e82d91ae110eeaac6b3a3
Caueh Oliveira <12563720+CauehCraft@users.noreply.github.com>
2024-03-13 16:51:25 -0300

    Update Empresa.java

--- Empresa/Empresa.java
+++ Empresa/Empresa.java
@@ -3,8 +3,8 @@ public class Empresa {
 	public static void main(String args[]) {
 		PessoaFactory factory = new CadastroPessoas();
 
-		Pessoa pessoa1 = factory.getPessoa("Funcionario");
-		Pessoa pessoa2 = factory.getPessoa("Gerente");
+		Pessoa pessoa1 = factory.criarPessoa("Funcionario");
+		Pessoa pessoa2 = factory.criarPessoa("Gerente");
 
 		pessoa1.exibirInfo();
 		pessoa2.exibirInfo();
